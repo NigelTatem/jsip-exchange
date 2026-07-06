e16c9c4bd80331f0940ae68408987bef24d29169
nigeltatem2@gmail.com <ubuntu@ip-172-31-29-102.us-east-2.compute.internal>
2026-07-06 18:05:49 +0000

    Copy Part 3 pathological bots + scenarios from the group repo
    Bring in the group's bots (book_filler, cancel_storm, noise_trader,
    slow_consumer, spammer) and scenarios (book_fill, cancel_storm, order_spam,
    slow_consumers). Register them in jsip_bots.{ml,mli} (keeping
    Market_maker_bot) and jsip_scenarios.ml, and add their tests to test_bots.ml
    -- adapting the one direct Bot_runtime.create call to the solo repo's
    ~dispatch_submit / ~dispatch_cancel labels.

--- app/bots/src/jsip_bots.ml
+++ app/bots/src/jsip_bots.ml
@@ -1,2 +1,6 @@
-(*_ Intentionally left blank *)
 module Market_maker_bot = Jsip_market_maker.Market_maker_bot
+module Book_filler = Book_filler
+module Cancel_storm = Cancel_storm
+module Noise_trader = Noise_trader
+module Slow_consumer = Slow_consumer
+module Spammer = Spammer
--- app/bots/src/jsip_bots.mli
+++ app/bots/src/jsip_bots.mli
@@ -1,1 +1,6 @@
 module Market_maker_bot = Jsip_market_maker.Market_maker_bot
+module Book_filler = Book_filler
+module Cancel_storm = Cancel_storm
+module Noise_trader = Noise_trader
+module Slow_consumer = Slow_consumer
+module Spammer = Spammer
