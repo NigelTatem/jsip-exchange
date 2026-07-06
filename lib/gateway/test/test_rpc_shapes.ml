368af4d7bbc7a6afb554b8b7a45112de200774e9
nigeltatem2@gmail.com <ubuntu@ip-172-31-29-102.us-east-2.compute.internal>
2026-07-06 17:50:18 +0000

    types: group aggressor_client_order_id with the aggressor fields in Fill.t
    Move aggressor_client_order_id up next to aggressor_participant/aggressor_side
    so all aggressor fields are contiguous. Reordering a bin_io record changes both
    its shape digest and its sexp field order, so update the streaming-RPC
    update_response digests in test_rpc_shapes and the Fill sexp in
    test_bot_runtime accordingly.

--- lib/gateway/test/test_rpc_shapes.ml
+++ lib/gateway/test/test_rpc_shapes.ml
@@ -69,7 +69,7 @@ let%expect_test "session feed RPC" =
     {|
     (Streaming_rpc (query 86ba5df747eec837f0b391dd49f33f9e)
      (initial_response 86ba5df747eec837f0b391dd49f33f9e)
-     (update_response 1b5a524d38b5b033deff3800023eafe7)
+     (update_response 2487a6fbe1cc9ae8a9306ccec1257362)
      (error 52966f4a49a77bfdff668e9cc61511b3))
     |}];
   return ()
@@ -97,7 +97,7 @@ let%expect_test "market-data RPC" =
     {|
     (Streaming_rpc (query 296be80010ace497614f92952e5510c4)
      (initial_response 86ba5df747eec837f0b391dd49f33f9e)
-     (update_response 1b5a524d38b5b033deff3800023eafe7)
+     (update_response 2487a6fbe1cc9ae8a9306ccec1257362)
      (error 52966f4a49a77bfdff668e9cc61511b3))
     |}];
   return ()
@@ -112,7 +112,7 @@ let%expect_test "audit-log RPC" =
     {|
     (Streaming_rpc (query 86ba5df747eec837f0b391dd49f33f9e)
      (initial_response 86ba5df747eec837f0b391dd49f33f9e)
-     (update_response 1b5a524d38b5b033deff3800023eafe7)
+     (update_response 2487a6fbe1cc9ae8a9306ccec1257362)
      (error 52966f4a49a77bfdff668e9cc61511b3))
     |}];
   return ()
